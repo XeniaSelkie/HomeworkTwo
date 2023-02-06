6b9bcc3471027f9486c0d4f349c40e1fb763fcf5
Sana <mypostoffice94@gmail.com>
2023-02-06 15:08:08 +0300

    Add Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write ("Введите номер дня недели ");
+if (int.TryParse(Console.ReadLine(), out int day))
+{
+    switch (day)
+    {
+        case 1:
+            Console.WriteLine("Это будний день");
+            break;
+        case 2:
+            Console.WriteLine("Это будний день");
+            break;
+        case 3:
+            Console.WriteLine("Это будний день"); 
+            break;
+        case 4:
+            Console.WriteLine("Это будний день"); 
+            break;
+        case 5:
+            Console.WriteLine("Это будний день"); 
+            break;
+        case 6:
+            Console.WriteLine("Это Это выходной день"); 
+            break;
+        case 7:
+            Console.WriteLine("Это выходной день");
+            break;
+        default:
+            Console.WriteLine("Такого дня недели нет");    
+            break;                                
+    }   
+}
+else
+{
+    Console.WriteLine("Ошибка ввода, пожалуйста, повторите попытку");
+}
